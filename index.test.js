b9dd5ad0df97f4858b443cc78aafb43c297e127b
Jan Bodnar <jan.bodnar@gmail.com>
2025-07-04 15:15:16 +0200

    modified

--- index.test.js
+++ index.test.js
@@ -1,17 +1,23 @@
 import { test, expect } from "bun:test";
-
-const url = "http://localhost:3000";
+import server from "./index.js";
 
 test("GET /", async () => {
-  const res = await fetch(url);
+  const res = await server.fetch(new Request("http://localhost:3000/"));
   expect(res.status).toBe(200);
   const text = await res.text();
   expect(text).toBe("Bun!");
 });
 
 test("GET /greet", async () => {
-  const res = await fetch(`${url}/greet`);
+  const res = await server.fetch(new Request("http://localhost:3000/greet"));
   expect(res.status).toBe(200);
   const text = await res.text();
   expect(text).toBe("Hello, world!");
+});
+
+test("GET /today", async () => {
+  const res = await server.fetch(new Request("http://localhost:3000/today"));
+  expect(res.status).toBe(200);
+  const text = await res.text();
+  expect(text).contains("Today is");
 });
